d096ad80ddd4f74d32f9fe49a297e4cfc4b7da66
alex <alexbnk@gmail.com>
2017-02-28 15:14:31 +0200

    updates on the fly and deletes entries with button

--- appCtrl.js
+++ appCtrl.js
@@ -1,5 +1,6 @@
 
-app.controller('directoryCtrl', function($scope,directoryService){
-$scope.people = directoryService.people;
-
+app.controller('directoryCtrl', function($scope,directoryFactory){
+$scope.people = directoryFactory.people;
+$scope.change = directoryFactory.change;
+$scope.delete = directoryFactory.delete;
 });
--- appMdl.js
+++ appMdl.js
@@ -1,20 +1,31 @@
-app.service('directoryService', function () {
+app.factory('directoryFactory', function () {
 
-  var people = [
-{firstName: "Alex",
-lastName : "Benedek"},
-{firstName: "Miri",
-lastName : "Benedek"},
-{firstName: "Itay",
-lastName : "Benedek"},
-{firstName: "Noam",
-lastName : "Benedek"},
-{firstName: "Paul",
-lastName : "Benedek"},
-{firstName: "Tal",
-lastName : "Benedek"}
+var change = function(id,fn,ln) {
+      console.log(id,fn,ln);
+      //people.splice(id,1,{firstName:firstName, lastName:lastName);
+};
+var deleteThis = function(id) {
+  people.splice(id,1);
+};
+
+var people = [
+{firstName: "Bob",
+lastName : "Longbeard"},
+{firstName: "John",
+lastName : "The Ripper"},
+{firstName: "Olivia",
+lastName : "Newton"},
+{firstName: "Jeff",
+lastName : "Barsky"},
+{firstName: "Jason",
+lastName : "Donovan"},
+{firstName: "Kylie",
+lastName : "Minouge"}
   ];
 
-return {people: people};
+return {people: people,
+        change: change,
+        delete: deleteThis
+  };
 
 })
